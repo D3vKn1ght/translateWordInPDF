9a5fe873563655ba6aa73a46f5a45149bc128822
trung501 <gotrungogle@gmail.com>
2022-04-26 19:09:27 +0700

    dict to csv

--- writeDictToTxt.py
+++ writeDictToTxt.py
@@ -9,16 +9,25 @@ class Dict:
 
     def save(self):
         np.save(self.nameDict, self.dictionary)
+        print("Ghi xong dict.npy")
 
     def load(self):
         self.dictionary = np.load(self.nameDict, allow_pickle='TRUE').item()
+        print("Co", len(self.dictionary), "tu trong dict")
+
+    def writeCSV(self):
+        if self.dictionary:
+            with open('dict.csv', 'w', encoding="utf-8") as f:
+                for key, value in self.dictionary.items():
+                    f.write(key + ',' + value + '\n')
+            print("Ghi xong dict.csv")
 
 
 dct = Dict()
-txt = ""
-for x in dct.dictionary:
-    txt += str(x)+" : "+str(dct.dictionary[x])+"\n"
-with open("dict.txt", "w", encoding="utf-8") as f:
-    f.write(txt)
+# txt = ""
+# for x in dct.dictionary:
+#     txt += str(x)+" : "+str(dct.dictionary[x])+"\n"
+# with open("dict.txt", "w", encoding="utf-8") as f:
+#     f.write(txt)
 
-print("Co", len(dct.dictionary), "tu trong dict")
+dct.writeCSV()
